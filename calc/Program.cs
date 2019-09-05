8c2322e9bfeb45f435c3da87da27d61224a94b82
chaitanya krishna <chaitanya.krishna@nathcorp.com>
2019-09-05 15:43:45 +0800

    changes yton

--- calc/Program.cs
+++ calc/Program.cs
@@ -71,7 +71,7 @@ namespace calc
             string ans;
             Console.WriteLine("Want to continue:(Y/N)");
             ans = Console.ReadLine();
-            if (ans == "y")
+            if (ans == "Y")
             {
                 goto label;
             }
